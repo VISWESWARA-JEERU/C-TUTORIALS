9acc7beb85f0f50b6a4655ae43025026f1bb890e
VISWESWARA-JEERU <jvsks28@gmail.com>
2024-07-28 22:12:10 +0530

    program added

---
+++ Trees/basictree.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node* create();
+struct node 
+    {
+        int data;
+        struct node *left,*right;
+    };
+   
+void main(){
+    
+   struct node *root;
+     root = 0;
+     create();
+
+};
+ struct node* create() 
+ {   
+     struct node *newnode;
+     newnode = (struct node*) malloc(sizeof(struct node));
+    // int x;exit
+     printf("\n enter the data value and enter -1 for no node");
+     scanf("%d",&newnode->data);
+     if(newnode->data==-1){
+        return 0;
+     }
+    // newnode->data=x;
+     printf("\n enter data for left child %d ",newnode->data);
+     newnode->left =create();
+     printf("\n enter data for right child %d ",newnode->data);
+     newnode->right =create();
+
+
+ }  
+
